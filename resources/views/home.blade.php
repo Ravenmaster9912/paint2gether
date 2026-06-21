03bac0d9a79ad11f1c2a9d86ef3d0f2bc14bd5ea
Ravenmaster9912 <theshidori@gmail.com>
2026-06-20 19:40:33 -0600

    home changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Home</title>
 </head>
-<body>
-    <h1>Lobby</h1>
-    <p>Este va a ser el lobby xd</p>
-</body>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-@stack('scripts')
-
+    <body style="background-color: #cccccc;">
+        <div class="btn btn-secondary" style="margin:10px;">Opciones</div>
+        <div class="row">
+            <div class="col" style="height: 700px; width:90%; background-color: white;">holas</div>
+            <div class="col" style="text: left;">
+                <div class="row">
+                    <a class="btn btn-secondary" href="#">Lapiz</a>
+                    <a class="btn btn-secondary" href="#">Borrador</a>
+                    <a class="btn btn-secondary" href="#">Gotero</a>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        @stack('scripts')
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\interfaceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home',[interfaceController::class, 'index']);
+Route::get('/', [interfaceController::class, 'index']);
