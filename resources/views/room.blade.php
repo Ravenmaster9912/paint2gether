ffa874bb2c561239e5d112ce91b4697730d374c8
Ravenmaster9912 <theshidori@gmail.com>
2026-06-26 13:58:51 -0600

    home interface modified

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -7,43 +7,57 @@
 @push('styles')
     <style>
         body{
-            margin: 2%;
-            background-color: #ccc;
+            margin: 0% 0% 0% 2%;
+            background-color: #efefef;
         } 
         canvas{
+            /*border: 1px solid black;*/
+        }
+        #canvas{
+            height: 55em;
+            border: 1px solid black;
+            background-color: white;
+        }
+
+        .color{
+            width: 50px;
+            height: 50px;
             border: 1px solid black;
         }
-        figure{
-            border: 1px solid black;
+        div{
         }
     </style>   
 @endpush
 
-@section('content')
-    <nav class="navbar navbar-expand-lg">
-        <div class="btn btn-secondary">Opciones</div>
-        <div class="btn btn-secondary"> OPC2</div>
-        <div class="btn btn-secondary"> OPC3</div>
-    </nav>   
-
-    <div class="row justify-content-md-center">
-        <div id="canvas" class="col-10">
-            <canvas class="col" id="canv" height="auto" width="auto">xd</canvas>
-        </div>
-        <!--<div class="col" style="height: 700px; width:90%; background-color: white;">holas</div>-->
-        <div class="col" style="text: left;">
-            <div class="row text-center" style="margin: 1% 10% 0% 10%;">
-                <a class="btn btn-secondary" href="#">Lapiz</a>
-                <a class="btn btn-secondary" href="#">Borrador</a>
-                <a class="btn btn-secondary" href="#">Gotero</a>
-                <p>Color 1</p>
-                <figure class="figure">
-                    xd
-                </figure>
-                <p>Color 2</p>
-                <figure class="figure">
-                    xd
-                </figure>
+@section('content')  
+    <div class="container-fluid">
+        <div class="d-flex flex-row">
+            <div id="canvas" class="flex-grow-1 vw-100">
+                <nav class="navbar">
+                    <a class="btn"><b>Opciones</b></a>
+                    <a class="btn" data-bs-toggle="popover" data-bs-title="Users list" data-bs-content="Users" data-bs-placement="left"><b>Usuarios</b></a>
+                </nav> 
+                <canvas id="canv" height="830" width="1680">xd</canvas>
+            </div>
+            <!--<div class="col" style="height: 700px; width:90%; background-color: white;">holas</div>-->
+            <div class="d-flex align-items-start flex-column flex-fill">
+                <div class="d-flex flex-column mt-5 gap-3 mx-5">
+                    <a class="btn btn-secondary p-2" href="#">Lapiz</a>
+                    <a class="btn btn-secondary p-2" href="#">Borrador</a>
+                    <a class="btn btn-secondary p-2" href="#">Gotero</a>
+                </div>
+                <div class="d-flex flex-column mx-auto mt-auto mb-5 justify-content-center text-center">
+                    <p></p>
+                    <p>Color 1</p>
+                    <div class="color" data-bs-toggle="popover" data-bs-title="Colors" data-bs-content="Colores xd" data-bs-placement="left">
+                        .
+                    </div>
+                    <p></p>
+                    <p>Color 2</p>
+                    <div class="color" data-bs-toggle="popover" data-bs-title="Colors" data-bs-content="Colores xd" data-bs-placement="left">
+                        .
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -63,5 +77,8 @@
         });
 
         canvas.add(rectangulo);
+
+        const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+        const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
     </script>
 @endpush
